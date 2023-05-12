e79368aff15d1ca71f9b7bbdf709d5b4da7546cb
hwu27 <huw029@ucsd.edu>
2023-05-11 19:51:00 -0700

    update 1

--- grade.sh
+++ grade.sh
@@ -8,6 +8,25 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f student-submission/ListExamples.java ]]
+then 
+    cp student-submission/ListExamples.java grading-area/
+    cp GradeServer.java grading-area/
+    cp Server.java grading-area/
+    cp TestListExamples.java grading-area/
+
+
+    javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" grading-area/*.java > output.txt 2>&1
+    if [[ $? == 1 ]]
+    then
+        cat output.txt
+    fi 
+    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore grading-area/TestListExamples.java > test.txt 2>&1
+
+else
+    echo Please provide the correct files!
+    exit 1
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
